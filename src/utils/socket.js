3edef2c2ff12fa6103565428ebf96e7ef9b2d886
dolahee <155641@naver.com>
2023-04-08 12:45:17 +0900

    수정

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-export const socket = io('http://localhost:4000/', {
+export const socket = io('https://dolahee.github.io/MafiaGame/', {
   cors: { origin: '*' },
   transports: ['websocket'],
 });
